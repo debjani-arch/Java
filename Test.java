b65f337870ba12e8d6e85563a05d78b6f60f43d7
Debjani De <debjanide2022@gmail.com>
2023-09-04 14:16:21 +0530

    Tes  Java Basics

---
+++ Test.java
@@ -0,0 +1,69 @@
+/*public class Test{
+    public static void main (String[]args){
+      int a = 2;
+      int b = +2;
+      int c = -2;
+  
+     // int sum = a+b;
+     // int diff = a-b;
+      //int mul = a*b;
+  
+      System.out.println(a++);
+      System.out.println(b++);
+      System.out.println(a--);
+      System.out.println(c++);
+      
+      
+      
+    }
+  }(Examples)*/
+
+ /*  public class Test{
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 200;
+        int diff = a-b;
+        
+        System.out.println(diff);
+    }
+        }(Crio Prob 1 and 2 solved)*/
+
+
+
+
+//(3)cHECK TWO INT ARE EQUAL IN JAVA OR NOT
+//WILL USE ARITHMETIC OPERATOR
+
+/*import java.util.*;
+public class Test{
+    public static void main(String[] args) {
+        int fisrtNum = 11;
+        int secondNum = 10;
+
+        if((fisrtNum - secondNum)== 0)
+        System.out.println("All Nums are Equal");
+        else
+        System.out.println("Not Equal");
+
+
+    }
+
+
+}*/
+
+import java.util.*;
+public class Test{
+    public static void main(String[] args) {
+        int fisrtNum = 101;
+        int secondNum = 1001;
+
+        if((fisrtNum == secondNum))
+        System.out.println("All Nums are Equal");
+        else
+        System.out.println("Not Equal");
+
+
+    }
+
+
+}
